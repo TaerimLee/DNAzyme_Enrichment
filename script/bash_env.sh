4ee94b2b7a1c9412f5d15a9b4f5f72da4e226d49
taerim <leetaerim1@gmail.com>
2026-06-29 15:51:06 -0500

    .gitignore updated. add directory structure.

--- script/bash_env.sh
+++
@@ -1,3 +0,0 @@
-conda create --name DNAzyme_v2 -c conda-forge -c bioconda pear fastqc fastx_toolkit cutadapt seqtk
-conda activate DNAzyme_v2
-conda install -c conda-forge -c bioconda jupyter pandas numpy seaborn matplotlib tabulate
